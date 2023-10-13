b49c4127880ec2b9f57b11314703ebe352f62b3d
AK73371\Luis Aneri Delgado <luis.aneri-delgado@akkodis.com>
2023-10-13 12:01:40 +0200

    Inicio microservicio usuarios

---
+++ proyecto01/usuario-servicio/src/main/java/com/raccoon/usuarioservicio/UsuarioServicioApplication.java
@@ -0,0 +1,13 @@
+package com.raccoon.usuarioservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServicioApplication.class, args);
+	}
+
+}
---
+++ proyecto01/usuario-servicio/src/test/java/com/raccoon/usuarioservicio/UsuarioServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raccoon.usuarioservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
